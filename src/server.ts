f56d7ed82f4f23e98596cda4347c73334a29cc43
siddharthpd <sidpd510@gmail.com>
2025-05-15 15:01:42 +0530

    feat: initialize Airbnb booking service with TypeScript and Express
    - Created package.json with necessary dependencies and scripts for development and production.
    - Added server configuration to load environment variables.
    - Implemented correlation ID middleware for request tracking.
    - Developed centralized error handling middleware.
    - Set up the main server file to initialize Express and apply middleware.
    - Created custom error classes for better error management.
    - Implemented async local storage for correlation ID management.
    - Configured TypeScript with strict type checking and output settings.

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { serverConfig } from './config';
+import { appErrorHandler } from './middlewares/error.middleware';
+import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware';
+
+
+const app = express();
+app.use(express.json());
+
+
+const PORT: number = serverConfig.PORT;
+
+app.use(attachCorrelationIdMiddleware);
+app.use(appErrorHandler);
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
