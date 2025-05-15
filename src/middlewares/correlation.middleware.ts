be13c9fac235b033e8338882432959107a00200a
siddharthpd <sidpd510@gmail.com>
2025-05-15 15:26:05 +0530

    feat: add logger configuration and correlation ID middleware

--- src/middlewares/correlation.middleware.ts
+++ src/middlewares/correlation.middleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { v4 } from "uuid";
-import { asyncLocalStorage } from "../utils/helpers/correlation.helpers.js";
+import { asyncLocalStorage } from "../utils/helpers/correlation.helpers";
 
 export const attachCorrelationIdMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const correlationId = v4();
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import { serverConfig } from './config';
-import { appErrorHandler } from './middlewares/error.middleware';
 import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware';
+import { appErrorHandler } from './middlewares/error.middleware';
+import { logger } from './config/logger.config';
 
 
 const app = express();
@@ -15,4 +16,5 @@ app.use(appErrorHandler);
 
 app.listen(PORT, async () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    logger.info('Database connection has been established successfully!')
 })
